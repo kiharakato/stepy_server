480aea60392dad6ee554b898a7907c7647c78ca0
kiharekato <kiharekato@gmail.com>
2016-07-18 01:19:35 +0900

    test

--- main.go
+++ main.go
@@ -4,10 +4,9 @@ import (
 	"net/http"
 	"os"
 	_ "stepy/controllers"
-	"strconv"
 )
 
 func main() {
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	http.ListenAndServe(":"+string(port), nil)
+	port := os.Getenv("PORT")
+	http.ListenAndServe(":"+port, nil)
 }
