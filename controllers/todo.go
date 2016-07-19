9d42747b5417c83fc55fe774a04f67ad7b4e4671
kiharekato <kiharekato@gmail.com>
2016-07-20 02:28:55 +0900

    listのroutingを設定

--- controllers/todo.go
+++ controllers/todo.go
@@ -10,12 +10,13 @@ type lists struct {
 	stepyHttp.APIResourceBase
 }
 
-func init() {
-	http.Handle("/list/", dispatcher())
+type items struct {
+	stepyHttp.APIResourceBase
 }
 
-func dispatcher() http.Handler {
-	return stepyHttp.Chain(stepyHttp.APIResourceHandler(lists{}))
+func init() {
+	http.Handle("/list/", stepyHttp.Chain(stepyHttp.APIResourceHandler(lists{})))
+	http.Handle("/list/item/", stepyHttp.Chain(stepyHttp.APIResourceHandler(items{})))
 }
 
 func (l lists) Get(req *http.Request) (stepyHttp.APIStatus, interface{}) {
@@ -28,6 +29,21 @@ func (l lists) Get(req *http.Request) (stepyHttp.APIStatus, interface{}) {
 }
 
 func (l lists) Post(req *http.Request) (stepyHttp.APIStatus, interface{}) {
+	title := req.PostFormValue("title")
+	list := db.CreateTodoList(title)
+	return stepyHttp.Success(http.StatusOK), list
+}
+
+func (i items) Get(req *http.Request) (stepyHttp.APIStatus, interface{}) {
+	if id, _ := stepyHttp.RequestGetParam(req, "id"); len(id) != 0 {
+		user := db.ReadUserByUuid(id)
+		return stepyHttp.Success(http.StatusOK), user
+	}
+
+	return stepyHttp.Fail(http.StatusNotFound, "invalid user id"), nil
+}
+
+func (i items) Post(req *http.Request) (stepyHttp.APIStatus, interface{}) {
 	title := req.PostFormValue("title")
 	list := db.CreateTodoList(title)
 	return stepyHttp.Success(http.StatusOK), list
