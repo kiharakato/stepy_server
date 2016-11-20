00e4324b95d917531fa65a261a6911b98639d166
kiharekato <kiharekato@gmail.com>
2016-11-20 16:51:06 +0900

    stateをenumに設定

--- db/item.go
+++ db/item.go
@@ -11,7 +11,7 @@ type Item struct {
 	ID         uint       `gorm:"primary_key" json:"id"`
 	Title      string     `gorm:"size:255;not null" json:"title"`
 	NotebookId uint       `gorm:"not null" json:"notebook_id"`
-	State      string     `gorm:"size ENUM('done', 'delete', 'run'); default: 'run'; not null"`
+	State      string     `gorm:"type: enum('done', 'delete', 'run'); default: 'run'; not null"`
 	CreatedAt  time.Time  `gorm:"not null; default: CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt  time.Time  `gorm:"not null; default: CURRENT_TIMESTAMP" json:"updated_at"`
 	DeletedAt  *time.Time `json:"deleted_at"`
